4cb802ab60987a2191db97546df819182ad1258b
0lgaP1 <volhapatapchuk@gmail.com>
2024-09-24 14:16:40 +0200

    finished the game with reset btn, reset settings

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
@@ -28,20 +36,39 @@ function getRandomComputerResult() {
       return `Computer wins! ${computerResult} beats ${userOption}`;
     }
   }
-  const playerScoreSpanElement = document.getElementById("player-score");
-const computerScoreSpanElement = document.getElementById("computer-score");
-const roundResultsMsg = document.getElementById("results-msg");
-const winnerMsgElement = document.getElementById("winner-msg");
-const optionsContainer = document.querySelector(".options-container");
-const resetGameBtn = document.getElementById("reset-game-btn");
-
 function showResults(userOption) {
   roundResultsMsg.innerText = getRoundResults(userOption);
   computerScoreSpanElement.innerText = computerScore;
   playerScoreSpanElement.innerText = playerScore;
+  if (playerScore === 3) {
+    winnerMsgElement.innerText = "Player has won the game!";
+    optionsContainer.style.display = "none";
+    resetGameBtn.style.display = "block";
 
-  
+  } else if (computerScore === 3) {
+    winnerMsgElement.innerText = "Computer has won the game!";
+    optionsContainer.style.display = "none";
+    resetGameBtn.style.display = "block";
+
+  }
 };
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+    resetGameBtn.style.display = "none";
+    optionsContainer.style.display = "block";
+    winnerMsgElement.innerText = "";
+    roundResultsMsg.innerText = "";
+  }
+
+  function playAgain() {
+    resetGame();
+    resetGameBtn.style.display = "none";
+  }
+
 const rockBtn = document.getElementById("rock-btn");
 const paperBtn = document.getElementById("paper-btn");
 const scissorsBtn = document.getElementById("scissors-btn");
@@ -57,3 +84,5 @@ paperBtn.addEventListener("click", function () {
 scissorsBtn.addEventListener("click", function () {
   showResults("Scissors");
 });
+
+resetGameBtn.addEventListener('click', playAgain);
